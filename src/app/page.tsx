3ac0e41adf6bc628dce83e7143e3ea89bc5fb0c1
md asharaf <718romanempire@gmail.com>
2024-08-14 20:51:00 +0530

    made changes in layout and page to use map container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import {
-  MapContainer,
-  TileLayer,
-  useMap,
-} from 'https://cdn.esm.sh/react-leaflet'
-export default function Home() {
-  return (
-    <>
+import dynamic from 'next/dynamic'
 
-    </>
-  );
+const DynamicMap = dynamic(() => import('../components/Map'), {
+  ssr: false,
+})
+
+export default function MapPage() {
+  return (
+      <DynamicMap />
+  )
 }
